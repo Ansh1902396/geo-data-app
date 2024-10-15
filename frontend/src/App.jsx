bb2825bc99b82b04f7b3588ef9e81e0ea1ce5e6b
aviral shukla <21ucs043@lnmiit.ac.in>
2024-10-15 11:38:50 +0530

    Updated Readme

---
+++ frontend/src/App.jsx
@@ -0,0 +1,45 @@
+// src/App.jsx
+import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import AccountManagement from "./pages/AccountManagement";
+import MainPage from "./pages/MainPage";
+import ErrorPage from "./pages/Error";
+const checkAuth = () => {
+  const token = localStorage.getItem("authToken");
+  return !!token;
+};
+// Create routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <MainPage />, // Main content after login
+      },
+      {
+        path: "/account",
+        element: <AccountManagement />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
